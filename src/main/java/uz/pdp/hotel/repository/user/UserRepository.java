d3f018bf10ecde5aa69f39dd820f2fc6aa72f656
Bilol665 <bilolsultan665@gmail.com>
2023-07-10 16:40:40 +0500

    Almost done

--- src/main/java/uz/pdp/hotel/repository/user/UserRepository.java
+++ src/main/java/uz/pdp/hotel/repository/user/UserRepository.java
@@ -1,8 +1,13 @@
 package uz.pdp.hotel.repository.user;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import uz.pdp.hotel.entity.user.UserEntity;
+import uz.pdp.hotel.entity.user.UserRole;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -10,4 +15,8 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     Optional<UserEntity> findUserEntityByEmail(String username);
+    @Transactional
+    @Modifying
+    @Query("update users u set u.role = :role where u.id = :id")
+    void block(@Param("role")UserRole role,@Param("id") UUID id);
 }
